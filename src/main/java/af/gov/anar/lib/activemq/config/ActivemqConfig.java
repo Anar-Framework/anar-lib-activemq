1037d981174a936fde2dc2ef1474e46b3a3b4db5
mohbadar <mohammadbadarhashimi@gmail.com>
2020-01-19 15:23:20 +0430

    dependencies

---
+++ src/main/java/af/gov/anar/lib/activemq/config/ActivemqConfig.java
@@ -0,0 +1,8 @@
+package af.gov.anar.lib.activemq.config;
+
+
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan(basePackages = {"af.*"})
+public class ActivemqConfig {
+}
